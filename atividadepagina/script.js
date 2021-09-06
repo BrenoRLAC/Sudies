e4aadaa738442d2b2bc9a7df45fbb18a03857aaf
Breno Rodrigues <75743151+BrenoRLAC@users.noreply.github.com>
2021-09-06 11:40:05 -0300

    Optimization

--- atividadepagina/script.js
+++ atividadepagina/script.js
@@ -1,16 +1,16 @@
 function formulario() {
     let form = document.querySelector('form'); 
-    let p = document.querySelector('p');
+    let container = document.querySelector('.container');
     let nome = document.querySelector('#nome').value;
     let mensalidade = document.querySelector('#mensalidade').value;
-    let tempo = document.querySelector('#ano').value;  
+    let tempo = document.querySelector('#ano').value; 
     
-    // p.style.display = block;
+    if (container.style.display = 'none'){
+        container.style.display = 'block';
+    }
+     
    
-    let montante = mensalidade * (Math.pow((1.2), tempo));
+    let montante = mensalidade * (Math.pow((1.02), tempo));
     form.innerHTML = `Olá ${nome} voce guardando R$${mensalidade} todo mês, em ${ano.value} anos, terá o valor de ${montante.toFixed(2)} com juros compostos de 2% ao ano`
 
 }
-
- // formulario.style.display = none;
-    // parameter.get()
