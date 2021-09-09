01f521c194e46d72150ad2b39b60a052e0818c40
Breno Rodrigues <75743151+BrenoRLAC@users.noreply.github.com>
2021-09-09 15:28:13 -0300

    Update script.js

--- atividadepagina/script.js
+++ atividadepagina/script.js
@@ -1,16 +1,20 @@
 function formulario() {
-    let form = document.querySelector('form'); 
+    let form = document.querySelector('form');
     let container = document.querySelector('.container');
     let nome = document.querySelector('#nome').value;
     let mensalidade = document.querySelector('#mensalidade').value;
-    let tempo = document.querySelector('#ano').value; 
-    
-    if (container.style.display = 'none'){
+    let tempo = document.querySelector('#ano').value;
+
+    if (container.style.display = 'none') {
         container.style.display = 'block';
     }
-     
-   
-    let montante = mensalidade * (Math.pow((1.02), tempo));
-    form.innerHTML = `Olá ${nome} voce guardando R$${mensalidade} todo mês, em ${ano.value} anos, terá o valor de ${montante.toFixed(2)} com juros compostos de 2% ao ano`
 
-}
+    // let composto = 0;
+    // composto = mensalidade * Math.pow((1.02), tempo);
+
+    // let conta = 0;
+    // conta = composto * (tempo * 12);
+
+    form.innerHTML = `Olá ${nome} voce guardando R$${mensalidade} todo mês, em ${tempo} anos, terá o valor de com juros compostos de 2% ao ano`
+
+}                   
